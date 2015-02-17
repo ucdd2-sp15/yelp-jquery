65d11f387395ffceb16511192339fa870aac62b9
BrianNewsom <brian.newsom@colorado.edu>
2015-02-17 09:58:58 -0700

    Fix user list and add user view and template

--- contents/js/user/list.js
+++ contents/js/user/list.js
@@ -9,13 +9,13 @@ function listUsers(){
         $.get("/yelp-jquery/templates/listUsers.jade", function(template) {
 
             // render the template
-            var html = jade.render(template, {items: users})            
+            var html = jade.render(template, {items: users})
 
             // assign the rendered html to the dom element whose id is #list
             $("#list").html(html)
 
             // load the first user to view
-            //viewUser(users[0].user_id)
+            viewUser(users[0].user_id)
 
         })
 
