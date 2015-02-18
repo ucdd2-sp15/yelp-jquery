2653a9b1f762aff883c7a36750683d5ed06931fe
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-02-18 11:18:13 -0700

    Added the list restaurants js file and a temporary jade file

--- contents/js/restaurant/list.js
+++ contents/js/restaurant/list.js
@@ -1,4 +1,21 @@
 function listRestaurants(){
 
-    alert('todo')
+    console.log('listing restaurants')
+
+    $.get("/yelp-jquery/data/restaurants.json.data", function(data) {
+
+        var restaurants = JSON.parse(data)
+        console.log(restaurants)
+
+        $.get("/yelp-jquery/templates/listRestaurants.jade", function(template) {
+
+            var html = jade.render(template, {items: restaurants})
+
+            $("#list").html(html)
+
+            viewRestaurant(restaurants[0].business_id)
+
+        })
+
+    })
 }
