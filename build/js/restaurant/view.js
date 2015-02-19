b85a707be5986ad670bdf8aced30d9797c04586c
Jake Smith <jakehockey10@gmail.com>
2015-02-18 18:44:06 -0700

    getting stuff ready

--- build/js/restaurant/view.js
+++ build/js/restaurant/view.js
@@ -1,15 +1,15 @@
-function viewRestaurant(business_id){
+function viewRestaurant(business_id) {
 
     console.log('viewing restaurant id = ', business_id);
 
     $.get("/yelp-jquery/data/restaurants.json.data", function(data) {
 
         var restaurants = JSON.parse(data);
-        var restaurant = _.find(restaurants, {business_id: business_id});
+        var restaurant = _.find(restaurants, { business_id: business_id });
 
         $.get("/yelp-jquery/templates/viewRestaurant.jade", function(template) {
 
-            var html = jade.render(template, {item: restaurant});
+            var html = jade.render(template, { item: restaurant });
 
             $("#details").html(html)
 
