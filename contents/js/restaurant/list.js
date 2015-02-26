66253965705da053e166609c8f686101883b284e
BrianNewsom <brian.newsom@colorado.edu>
2015-02-26 10:38:26 -0700

    List restaurants from yelp api

--- contents/js/restaurant/list.js
+++ contents/js/restaurant/list.js
@@ -2,14 +2,14 @@ function listRestaurants(){
 
     console.log('listing restaurants')
 
-    $.get("/yelp-jquery/data/restaurants.json.data", function(data) {
+    $.get("https://lit-bayou-6850.herokuapp.com/search/term=food&location=Boulder", function(data) {
 
-        var restaurants = JSON.parse(data)
+        var restaurants = data.businesses
 
         $.get("/yelp-jquery/templates/listRestaurants.jade", function(template) {
 
             // render the template
-            var html = jade.render(template, {items: restaurants})            
+            var html = jade.render(template, {items: restaurants})
 
             // assign the rendered html to the dom element whose id is #list
             $("#list").html(html)
