e79e5335ffd234ff03452994ca278c37b65f5bce
Jake Smith <jakehockey10@gmail.com>
2015-02-25 19:28:18 -0700

    add references and apiacces.js

---
+++ contents/js/apiaccess.js
@@ -0,0 +1,4 @@
+var consumer_key = { consumer_key: 'UWobTMG9B3mAW5FXp-PNaw'};
+var consumer_secret = { consumer_secret: 'gi95WHKwfVe6IUodPU9c1Ks2_Vs'};
+var token = { token: '4W6L2n8dIF3gC2-VHYnnojDvZg42ulRv'};
+var token_secret = { token_secret: 'I7uZw5CErSd0my2gS3s5-FohMcs'};
