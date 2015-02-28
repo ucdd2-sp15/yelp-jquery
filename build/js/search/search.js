ee45b756cb9cc757126386f2aeb02e9b77bfe281
Jake Smith <jakehockey10@gmail.com>
2015-02-28 13:38:02 -0700

    added error handling

--- build/js/search/search.js
+++ build/js/search/search.js
@@ -2,38 +2,46 @@ function search(term, location) {
 
     // Get the data through our handy-dandy server
     $.get("https://rocky-everglades-5328.herokuapp.com/search/term=" + term + "&location=" + location, function(data) {
+        var html;
+        var error;
 
-        // Get our local search template to render
-        $.get("/yelp-jquery/templates/search.jade", function(template) {
+        // If data.data exists
+        if ('data' in data) {
 
-            var html;
-            var error;
-            if ('data' in data) {
-                var parse = JSON.parse(data.data);
-                if (parse.error) error = parse.error;
-            }
-            if (error) {
-                $.get('/yelp-jquery/templates/error.jade', function(template) {
-                    html = jade.render(template, {item: error});
-                    $('#search').html(html);
-                    $('#business').html("<img src='./images/Hd_computer_guy_meme_by_zapgod16-d4t2jh3.png'></img>");
-                });
-            } else if (data.businesses == undefined) {
-                $.get('/yelp-jquery/templates/empty.jade', function(template) {
-                    html = jade.render(template, {});
-                    $('#search').html(html);
-                    $('#business').html("<img src='./images/Hd_computer_guy_meme_by_zapgod16-d4t2jh3.png'></img>");
-                });
-            } else {
-                // render the template
+            // data.data might be a string
+            var parse = JSON.parse(data.data);
+
+            // Get the error if it exists
+            if (parse.error) error = parse.error;
+        }
+
+        // If error exists
+        if (error) {
+
+            // Get the error jade template
+            $.get('/yelp-jquery/templates/error.jade', function(template) {
+                html = jade.render(template, {item: error});  // Display the error
+                $('#search').html(html);
+                $('#image').html("<img src='./images/Hd_computer_guy_meme_by_zapgod16-d4t2jh3.png'/>");
+            });
+        // else if there is no businesses array
+        } else if (data.businesses == undefined) {
+
+            // Get the empty jade template (no results were found)
+            $.get('/yelp-jquery/templates/empty.jade', function(template) {
+                html = jade.render(template, {});  // No dynamic variables in template
+                $('#search').html(html);
+                $('#image').html("<img src='./images/Hd_computer_guy_meme_by_zapgod16-d4t2jh3.png'/>");
+            });
+        } else {
+
+            // Get our local search template to render
+            $.get("/yelp-jquery/templates/search.jade", function(template) {
                 html = jade.render(template, {items: data.businesses});
-
-                // assign the rendered html to the dom element whose id is #list
                 $("#search").html(html);
-
-                // load the first doctor to view
-                business(data.businesses[0].id)
-            }
-        })
+                $('#image').html();
+                business(data.businesses[0].id);  // Default to the first business found for the #business element
+            });
+        }
     })
 }
--- contents/js/business/business.js
+++ contents/js/business/business.js
@@ -1,8 +1,10 @@
+// TODO: Add some comments.
 function business(id){
     $.get("https://rocky-everglades-5328.herokuapp.com/business/" + id, function(data) {
 
         $.get("/yelp-jquery/templates/business.jade", function(template) {
 
+            console.log(data);
             var html = jade.render(template, {item: data});
 
             $("#business").html(html)
