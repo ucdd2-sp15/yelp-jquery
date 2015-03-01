78e6cb92dbb5b9de3a3be31e8c216b4c51d69bb3
wuzhi1023 <woozhh@gmail.com>
2015-02-28 21:58:37 -0700

    bug fixed

--- contents/js/app.js
+++ contents/js/app.js
@@ -1,12 +1,18 @@
 $( "#doctors" ).on( "click", function( event ) {
+	$("#searchlist").empty()
+    $("#ui").empty()
     listDoctors()
 })
 
 $( "#users" ).on( "click", function( event ) {
+	$("#searchlist").empty()
+    $("#ui").empty()
     listUsers()
 })
 
 $( "#restaurants" ).on( "click", function( event ) {
+	$("#searchlist").empty()
+    $("#ui").empty()
     listRestaurants()
 })
 
