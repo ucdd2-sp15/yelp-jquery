5efb6ebd8ad58bd2854e22f30fb4c668b639fa9d
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-03-01 13:09:04 -0700

    business

--- contents/js/business/index.js
+++ contents/js/business/index.js
@@ -1,36 +1,12 @@
 var business = {
     
-    searchByName: function(name) {
+    searchById: function(id) {
 
-        $.get("http://api.yelp.com/v2/business/?name=" + name, apikey, function(data) {
-
-            $.get("/yelp-jquery/contents/js/business/list.jade", function(template) {
-                var html = jade.render(template, {
-                    data: data
-                })
-                $("#list").html(html)
-            })
-        })
-    },
-    
-    searchByCategory: function(category) {
-
-        $.get("http://api.yelp.com/v2/business/?categories=" + category, apikey, function(data) {
-
-            $.get("/yelp-jquery/contents/js/business/list.jade", function(template) {
-                var html = jade.render(template, {
-                    data: data
-                })
-                $("#list").html(html)
-            })
-        })
-    },
-    
-    searchByLocation: function(location) {
-
-        $.get("http://api.yelp.com/v2/business/?location=" + location, apikey, function(data) {
-
-            $.get("/yelp-jquery/contents/js/business/list.jade", function(template) {
+        // https://rocky-refuge-9596.herokuapp.com
+        // http://api.yelp.com/v2/business/
+        $.get("https://rocky-refuge-9596.herokuapp.com/business/" + id, function(data) {
+            console.log(data)
+            $.get("/yelp-jquery/js/business/list.jade", function(template) {
                 var html = jade.render(template, {
                     data: data
                 })
@@ -41,13 +17,14 @@ var business = {
     
     load: function() {
 
-        $.get("/yelp-jquery/contents/js/business/ui.jade", function(template) {
+        $.get("/yelp-jquery/js/business/ui.jade", function(template) {
             var html = jade.render(template)
-            $("#ui").html(html)
+            $("#list").html(html)
+            
         })
 
         // default search results
-        businesses.searchByName('Urban Curry')
-
+        $("#details").html("")
+        business.searchById('yelp-san-francisco')
     }
 }
