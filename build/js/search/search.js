4f72411feaf4f5dfa64d36ca7a46ce3aa5918efc
wuzhi1023 <woozhh@gmail.com>
2015-02-28 21:29:51 -0700

    Finished all, 50/50

---
+++ build/js/search/search.js
@@ -0,0 +1,46 @@
+var search = {
+
+    searchByTerm: function(term, location) {
+        $.get("https://yelp-server.herokuapp.com/search/term="+term+"&location="+location, function(data) {
+
+            $.get("/yelp-jquery/js/search/list.jade", function(template) {
+                var html = jade.render(template, {
+                    data: data
+                })
+                $("#searchlist").html(html)
+            })
+
+        })
+
+    },
+
+
+    searchByBusiness: function(business) {
+        $.get("https://yelp-server.herokuapp.com/business/" + business, function(data) {
+            console.log(data)
+            $.get("/yelp-jquery/js/search/listBusiness.jade", function(template) {
+                var html = jade.render(template, {
+                    item: data
+                })
+                $("#searchlist").html(html)
+            })
+
+        })
+
+    },
+
+
+
+    load: function() {
+
+        $.get("/yelp-jquery/js/search/ui.jade", function(template) {
+            var html = jade.render(template)
+            $("#ui").html(html)
+        })
+
+        // default search results
+        search.searchByBusiness('yelp-san-francisco')
+
+    }
+
+}
